2be9bb6075597f6e0b3755ef6d58ce10fcc81078
unpublished3 <chapagainsakshyam@gmail.com>
2026-02-04 07:19:43 +0545

    Ble notify serial data

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
 #include <Arduino.h>
+#include <NimBLEDevice.h>
+
+// data
+NimBLECharacteristic *serialChar;
 
 // put function declarations here:
-int myFunction(int, int);
+NimBLECharacteristic* setupBluetooth(NimBLEServer*);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  NimBLEDevice::init("Chronex");
+
+  NimBLEServer *pServer = NimBLEDevice::createServer();
+  serialChar =  setupBluetooth(pServer);
+  NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID("12345678-1234-1234-1234-123456789abc"); 
+  pAdvertising->setName("Chronex");
+  pAdvertising->start(); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  serialChar->setValue((uint32_t)millis());
+  serialChar->notify();
+  delay(1000);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) { return x + y; }
+NimBLECharacteristic* setupBluetooth(NimBLEServer* server) {
+  NimBLEService *service = server->createService("12345678-1234-1234-1234-123456789abc");
+  NimBLECharacteristic *pCharacteristic = service->createCharacteristic("87654321-4321-4321-4321-cba987654321", NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::NOTIFY);
+
+  service->start();
+  return pCharacteristic;
+}
